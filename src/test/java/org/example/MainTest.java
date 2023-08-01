d6f84dbb15bf97337917f281fdc2e052e6fdfd0a
SirTimme <tim.piecho@gmail.com>
2023-08-01 15:52:40 +0200

    added test for main class

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void mainPrintHelloWorld() {
+        Main.main(new String[]{});
+
+        assertEquals("Hello world!", outputStreamCaptor.toString().trim());
+    }
+}
