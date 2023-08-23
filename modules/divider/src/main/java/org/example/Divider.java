412f165ea3931bb857031b0dd0dc8ab8dfb9976a
Tim Piechowicz <tim.piechowicz@intrexx.com>
2023-08-23 11:21:10 +0200

    removes unnecessary symbol from code comment

--- modules/divider/src/main/java/org/example/Divider.java
+++ modules/divider/src/main/java/org/example/Divider.java
@@ -8,7 +8,7 @@ public class Divider {
      * Divides two integers
      *
      * @param a the numerator
-     * @param b the denominator. If the denominator is 0, the methods throws an <code>IllegalArgumentException</code>     *
+     * @param b the denominator. If the denominator is 0, the methods throws an <code>IllegalArgumentException</code>
      * @return the divided number
      */
     public float divide(int a, int b) {
